c8b29a2130bd103663a1049ce0fb3657d709e644
TStella <tommaso.stella@zalf.de>
2017-05-15 16:58:56 +0200

    Extended support for grid creation
    - "crop" and "year" output can be written in grids
    - different rotations can be plotted in separated grids

--- create-out-grid.py
+++ create-out-grid.py
@@ -2,51 +2,39 @@ import numpy as np
 import pandas as pd
 from collections import defaultdict
 
-#read file
-df = pd.read_csv("out/129_crop.csv")
-
-#df.append(df2, ignore_index=True)
-
-#subset for crop
-crop_df = df[df.crop == "mustard_"]
-
-#calculate summary out
-#average_LAI = crop_df["LAImax"].mean()
-
-#aggregate per cell and rotation
-cell_rotation = crop_df.groupby(["IDcell", "rotation"]).mean()
-
-#print cell_rotation.index
-print cell_rotation
-
-#retrieve values per cell/rotation
-cells = cell_rotation.index.get_level_values('IDcell').tolist()
-rotations = cell_rotation.index.get_level_values('rotation').tolist()
-
-#TODO find a better solution to get the single value of interest
-r_loc = {}
-for i in range(len(cells)):
-    r_loc[(cells[i], rotations[i])] = i
-
-c_loc = {}
-for i in range(len(cell_rotation.columns)):
-    c_loc[cell_rotation.columns[i]] = i
-
-plot_vars = ["cyclelength", "agb"]
-
-def update_grid_data(row, col, rot, out_var, offset_row = 282, offset_col = 0):
-    row_col = "{}{:03d}".format(row + offset_row, col + offset_col)
-    if (int(row_col), int(rot)) in r_loc:
-        r_index =  r_loc[(int(row_col), int(rot))]
-        grid_data[row][col] = cell_rotation.iloc[(r_index,c_index)]
+def map_indexes(aggr_data, cells, rotations=None):
+    r_loc = {}
+    c_loc = {}    
+    #rows
+    if rotations:
+        for i in range(len(cells)):
+            r_loc[(cells[i], rotations[i])] = i
     else:
-        grid_data[row][col] = -9999
+        for i in range(len(cells)):
+            r_loc[cells[i]] = i
+    #cols
+    for i in range(len(aggr_data.columns)):
+        c_loc[aggr_data.columns[i]] = i
+    
+    return r_loc, c_loc
 
-def write_grid_file(grid_data, out_var, rot):
-    out_file = "out/grids/" + str(out_var) + "_" + str(rot) + ".asc"
-    with(open(out_file, "w")) as _:    
-        #header
-        _.write(
+def update_grid_data(aggr_data, grid_data, row, col, r_loc, c_index, rot=None, offset_row=282, offset_col=0):
+    row_col = "{}{:03d}".format(row + offset_row, col + offset_col)
+    grid_data[row][col] = -9999
+    if rot:
+        if (int(row_col), int(rot)) in r_loc:
+            r_index =  r_loc[(int(row_col), int(rot))]
+            grid_data[row][col] = aggr_data.iloc[(r_index,c_index)]
+    else:
+        if int(row_col) in r_loc:
+            r_index =  r_loc[int(row_col)]
+            grid_data[row][col] = aggr_data.iloc[(r_index,c_index)]
+
+def write_grid_file(grid_data, cp_yr_tag, out_var, rot=""):
+        out_file = "out/grids/" + str(cp_yr_tag) + "_" + str(out_var) + "_" + str(rot) + ".asc"
+        with(open(out_file, "w")) as _:    
+            #header
+            _.write(
 """ncols        250
 nrows        241
 xllcorner    3280914.799999999800
@@ -54,30 +42,128 @@ yllcorner    5580000.500000000000
 cellsize     1000.000000000000
 NODATA_value  -9999
 """)
-        #grid
-        for row in range(n_rows):
-            for col in range(n_cols):
-                _.write(str(grid_data[row][col]) + " ")
-            _.write("\n")       
+            #grid
+            for row in range(n_rows):
+                for col in range(n_cols):
+                    _.write(str(grid_data[row][col]) + " ")
+                _.write("\n")
 
-n_rows = 241
-n_cols = 250
+def crop_rotation_grids(plot_vars, n_rows, n_cols):
 
-for rot in set(rotations):
+    #identify available crops
+    crops = set(df.crop)
+
+    #subset for crop
+    for cp in crops:
+        crop_df = df[df.crop == cp]
+
+        #aggregate per cell and rotation
+        aggr_data = crop_df.groupby(["IDcell", "rotation"]).mean()
+        #print aggr_data
+
+        #identify cells/rotations
+        cells = aggr_data.index.get_level_values('IDcell').tolist()
+        rotations = aggr_data.index.get_level_values('rotation').tolist()
+        
+        #map indexes
+        r_loc, c_loc = map_indexes(aggr_data, cells, rotations)
+        
+        for rot in set(rotations):
+            for out_var in plot_vars:
+                grid_data = defaultdict(lambda: defaultdict(dict)) #dict row, dict col
+                c_index = c_loc[out_var]
+                for row in range(n_rows):
+                    for col in range(n_cols):
+                        update_grid_data(aggr_data, grid_data, row, col, r_loc, c_index, rot)
+                write_grid_file(grid_data, cp, out_var, rot)
+                print("out grid for crop: " + str(cp) + " var: " + str(out_var) + " and rotation: " + str(rot) + " ready!")
+
+def crop_grids(plot_vars, n_rows, n_cols):
+    
+    #identify available crops
+    crops = set(df.crop)
+
+    #subset for crop
+    for cp in crops:
+        crop_df = df[df.crop == cp]
+
+        #aggregate per cell and rotation
+        aggr_data = crop_df.groupby(["IDcell"]).mean()
+        #print aggr_data
+
+        #retrieve values per cell/rotation
+        cells = aggr_data.index.get_level_values('IDcell').tolist()        
+
+        #map indexes
+        r_loc, c_loc = map_indexes(aggr_data, cells)
+
+        for out_var in plot_vars:
+            grid_data = defaultdict(lambda: defaultdict(dict)) #dict row, dict col
+            c_index = c_loc[out_var]
+            for row in range(n_rows):
+                for col in range(n_cols):
+                    update_grid_data(aggr_data, grid_data, row, col, r_loc, c_index)
+            write_grid_file(grid_data, cp, out_var)
+            print("out grid for crop: " + str(cp) + " var: " + str(out_var) + " ready!")
+
+def year_rotation_grids(plot_vars, n_rows, n_cols):
+    
+    #aggregate per cell and rotation
+    aggr_data = df.groupby(["IDcell", "rotation"]).mean()
+    #print aggr_data
+
+    #retrieve values per cell/rotation
+    cells = aggr_data.index.get_level_values('IDcell').tolist()
+    rotations = aggr_data.index.get_level_values('rotation').tolist()
+
+    #map indexes
+    r_loc, c_loc = map_indexes(aggr_data, cells, rotations)
+
+    for rot in set(rotations):
+        for out_var in plot_vars:
+            grid_data = defaultdict(lambda: defaultdict(dict)) #dict row, dict col
+            c_index = c_loc[out_var]
+            for row in range(n_rows):
+                for col in range(n_cols):
+                    update_grid_data(aggr_data, grid_data, row, col, r_loc, c_index, rot)
+            write_grid_file(grid_data, "yearly", out_var, rot)
+            print("out grid for yearly out var: " + str(out_var) + " and rotation: " + str(rot) + " ready!")
+
+def year_grids(plot_vars, n_rows, n_cols):
+    
+    #aggregate per cell and rotation
+    aggr_data = df.groupby(["IDcell"]).mean()
+    #print aggr_data
+
+    #retrieve values per cell/rotation
+    cells = aggr_data.index.get_level_values('IDcell').tolist()
+
+    #map indexes
+    r_loc, c_loc = map_indexes(aggr_data, cells)
+
     for out_var in plot_vars:
         grid_data = defaultdict(lambda: defaultdict(dict)) #dict row, dict col
         c_index = c_loc[out_var]
         for row in range(n_rows):
             for col in range(n_cols):
-                update_grid_data(row, col, rot, out_var)
-        write_grid_file(grid_data, out_var, rot)
-        print("out grid for var: " + str(out_var) + " and rotation: " + str(rot) + " ready!")
+                update_grid_data(aggr_data, grid_data, row, col, r_loc, c_index)
+        write_grid_file(grid_data, "yearly", out_var)
+        print("out grid for yearly out var: " + str(out_var) + " ready!")
 
+#read file(s)
+df_cp_129 = pd.read_csv("out/129_crop.csv")
+df_cp_142 = pd.read_csv("out/142_crop.csv")
 
-#test4 = cell_rotation.loc[cell_rotation.index['IDcell'] == '353141']
-#test3 = cell_rotation.loc[cell_rotation['year'] < 3531.41, 'agb']
-#test = cell_rotation.iloc[(0,1)]
-#print test
+frames = [df_cp_129, df_cp_142]
+df = pd.concat(frames)
 
-#agb = cell_rotation[(353141, 6110), 'agb']#[cell_rotation['IDcell'] == '353141', 'agb']
+plot_vars = ["agb"]
+n_rows = 241
+n_cols = 250
 
+#crop_rotation_grids(plot_vars, n_rows, n_cols)
+#crop_grids(plot_vars, n_rows, n_cols)
+year_rotation_grids(plot_vars, n_rows, n_cols)
+year_grids(plot_vars, n_rows, n_cols)
+
+print "finished!"
